dee17055bfe0b39bdad0b1fae16c21039da175d3
leo <wangsongyu_leo@outlook.com>
2018-03-22 08:01:37 +0800

    leo push

--- src/components/my-juanpi.vue
+++ src/components/my-juanpi.vue
@@ -1,6 +1,47 @@
 <template>
-	<div>
-		my-juanpi
+	<div id="box">
+		<header>
+			<div class="title">
+				<a href="javascript:;">
+					<i class="iconfont icon-jiantou1"></i>
+				</a>
+				<span>个人中心</span>
+			</div>
+			<div class="login">
+				<a href="javascript:;">注册</a>
+				<span> | </span>
+				<a href="javascript:;">登陆</a>
+			</div>
+		</header>
+		<div class="order">
+			<div class="orderbox">
+				<div class="myorder">
+					我的订单
+				</div>
+				<div class="allorder">
+					全部订单
+				</div>
+			</div>
+		</div>
+		<ul class="editlist">
+			<li>
+				<i class="iconfont icon-daifukuan"></i><br>
+				<span>待付款</span>
+			</li>
+			<li>
+				<i class="iconfont icon-daichengtuan"></i><br>
+				<span>待付款</span>
+			</li>
+			<li>
+				<i class="iconfont icon-daishouhuo"></i><br>
+				<span>待付款</span>
+			</li>
+			<li>
+				<i class="iconfont icon-tuikuan"></i><br>
+				<span>待付款</span>
+			</li>
+		</ul>
+		
 	</div>
 
 </template>
@@ -20,6 +61,96 @@
 
 
 <style type="text/css" lang="scss" scoped>
-	
+#box{
+	background:#f4f4f8
+}
+	header{
+		height:144px;
+		background:url('https://jp.juancdn.com/jpwebapp_v1/images_v1/user/user_center_bg.png') no-repeat center;
+		width:100%;
+		.title{
+			display:flex;
+			height:44px;
+			text-align:center;
+			line-height:44px;
+			font-size:18px;
+			color:white;
+			a{
+				width:15%;
+				height:100%;
+
+				i{
+					font-size:20px;
+					color:white;
+				}
+			}
+			span{
+				width:85%;
+				height:100%;
+				padding-right:10%
+			}
+		}
+		.login{
+			display:flex;
+			padding:0 12%;
+			a{	
+				width:48%;
+				text-align:center;
+				font-size:20px;
+				color:white;
+				line-height:100px;
+			}
+			span{
+				width:4%;
+				font-size:20px;
+				color:white;
+				line-height:100px;
+			}
+		}
+	}
+	.order{
+		padding:0 5%;
+		height:50px;
+		background:#fff;
+		.orderbox{
+			border-bottom:1px solid #ccc;
+			display:flex;
+			justify-content:space-between;
+			height:100%;
+			.myorder{
+				width:20%;
+				font-size:15px;
+				line-height:50px;
+				font-weight:500;
+				color:#3e3e3e;
+			}
+			.allorder{
+				width:20%;
+				font-size:14px;
+				line-height:50px;
+				color:#999;
+			}
+		}
+	}
+	.editlist{
+		display:flex;
+		background:#fff;
+		li{
+			flex:1;
+			text-align:center;
+			height:60px;
+			padding-top:20px;
+			i{
+				font-size:24px;
+				font-weight:500;
+				
+			}
+			span{
+				font-size:12px;
+				margin-top:10px;
+				display:inline-block;
+			}
+		}
+	}
 
 </style>
