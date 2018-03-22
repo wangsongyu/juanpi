a068b43fe2f96031154c3f960b9527d3efa7c5e8
wangjy <galialiga@163.com>
2018-03-22 10:16:52 +0800

    yoyo push vip

--- src/components/vip.vue
+++ src/components/vip.vue
@@ -1,18 +1,94 @@
 <template>
-	<div>
-		vip
+	<div class="all">
+		<swipe class="my-swipe">
+		  <swipe-item v-for="image in swipeimage" :key="image.x_record" class="swiper">
+		  	<img :src="image.pic">
+		  </swipe-item>
+		</swipe>
+		<div class="offer">
+			<a href="">
+				<img :src="offer" alt="">
+			</a>
+		</div>
+		<div class="newVip">
+			<a href="">
+				<img :src="newVip" alt="">
+			</a>
+		</div>
+		<div class="con4">
+			<img v-for="src in con4":src="src.pic" alt="">
+		</div>
+		<div class="con5">
+			<img v-for="every in con5" :src="every.pic" alt="">
+		</div>
+		<div class="rush">
+			<img :src="rush" alt="">
+		</div>
+		<div class="good">
+			<img :src="good" alt="">
+		</div>
+		<div class="navbox">
+			<ul class="navlist" v-draglist>	
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/recommend">推荐</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/paper">纸品</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/milk">牛奶</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/food">美食</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/baby">母婴</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/myhome">居家</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/wine">酒水</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/beautiful">美妆</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/device">电器</router-link>
+				<router-link tag="li" activeClass="activeCss" to="/vipnav/fruit">水果</router-link>
+			</ul>
+		</div>
+		<router-view></router-view>
 	</div>
 
 </template>
 
 
 <script type="text/javascript">
+import axios from 'axios'
+require('vue-swipe/dist/vue-swipe.css');
+import { Swipe, SwipeItem } from 'vue-swipe';
+import { Navbar, TabItem } from 'mint-ui';
 	export default {
 		name:'vip',
 		data(){
 			return{
-				
+				swipeimage:[],
+				datalist:[],
+				offer:'',
+				newVip:'',
+				con4:[],
+				con5:[],
+				rush:'',
+				good:'',
+				zonelist:[]
 			}
+		},
+		mounted(){
+			axios.get('/api/getSupermarketAds?cid=sumarket_sort&zy_ids=c4_l4_1456_1186_1220_1406_1184_1217_1371_5_128_106_51_18_1391&app_name=zhe&catname=').then(res=>{
+				console.log(res.data.adsInfo.block[0].multi_block)
+				this.datalist=res.data.adsInfo.block[0].multi_block
+				this.swipeimage=res.data.adsInfo.block[0].multi_block[0].data.child
+				this.offer=this.datalist[1].data[0].child[0].pic
+				this.newVip=this.datalist[2].data[0].child[0].pic
+				this.con4=this.datalist[3].data[0].child
+				this.con5=this.datalist[4].data[0].child
+				this.rush=this.datalist[5].data[0].child[0].pic
+				this.good=this.datalist[6].data[0].child[0].pic
+			})
+
+			axios.get('/api/getSupermarketGoods?cid=sumarket_sort&page=1&zhouyi_ids=p8_c4_l4_1456_1186_1220_1406_1184_1217_1371_5_128_106_51_18_1391&source=sumarket').then(res=>{
+				console.log(res.data.data.goods)
+				this.zonelist=res.data.data.goods 
+			})
+		},
+		components:{
+		    'swipe': Swipe,
+		    'swipe-item': SwipeItem
+
 		}
 	}
 
@@ -20,6 +96,84 @@
 
 
 <style type="text/css" lang="scss" scoped>
-	
+.all{height:2000px;}
+	.my-swipe {
+	  height:150px;
+	 	img{
+	 		width:100%;
+			height:100%;
+	 		display:block;
+	 	}
+	}
+	.offer{
+		height:40px;
+		width:100%;
+		img{
+			width:100%
+		}
+	}
+	.newVip{
+		display:flex;
+		width:100%;
+		height:180px;
+		img{
+			height:100%;
+			width:100%
+		}
+	}
+	.con4{
+		display:flex;
+		width:100%;
+		height:110px;
+		img{
+			flex:1;
+			width:50%;
+		}
+	}
+	.con5{
+		display:flex;
+		width:100%;
+		height:125px;
+		img{
+			width:33.3%
+		}
 
+	}
+	.rush{
+		width:100%;
+		display:flex;
+		height:80px;
+		img{
+			width:100%;
+		}
+	}
+	.good{
+		width:100%;
+		display:flex;
+		height:50px;
+		img{
+			width:100%;
+		}
+	}
+	.navbox{
+		width:100%;
+		height:40px;
+		background:#ff0;
+		position:relative;
+		overflow:hidden;
+		.navlist{
+			position:absolute;
+			top:0;
+			left:0;
+			display:flex;
+			width:160%;
+			li{
+				flex:1;
+				text-align:center;
+				line-height:40px;
+				font-size:15px;
+				font-weight:500;
+			}
+		}
+	}
 </style>
---
+++ src/components/vipnav/recommend.vue
@@ -0,0 +1,104 @@
+<template>
+	<div>
+		<ul class="homenav_list"
+		 v-infinite-scroll="loadMore"
+		 infinite-scroll-disabled="loading"
+		 infinite-scroll-distance="100"
+		 infinite-scroll-immediate-check=false
+		 >
+			<li class="homenav_li" v-for="data in datalist">
+				<img class="homenav_img" :src="data.pic_url" alt="">
+				<span class="homenav_price">{{data.priceList[0].text}}</span>
+				<span class="homenav_allprice">￥{{data.oprice}}</span>
+				<span class="homenav_sport">{{data.priceList[1].text}}</span>
+				<p class="homenav_vip">{{data.title}}</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script type="text/javascript">
+
+import axios from 'axios'
+import { Indicator } from 'mint-ui'
+	
+	export default {
+		name:'recommend',
+		data(){
+			return{
+				datalist:[],
+				loading:false,
+				current:1,
+				stop:''
+			}
+		},
+		mounted(){
+			Indicator.open('加载中...');
+			axios.get("/api/getGoods?page=1&zy_ids=p8_c4_l4_1456_1186_1220_1406_1184_1217_1371_5_128_106_51_18_1391&app_name=zhe&catname=xbsytj&flag=xbsytj").then(res=>{
+				// console.log(res.data.data.goods)
+				this.datalist=res.data.data.goods
+			})
+		},
+		methods:{
+
+			loadMore(){
+				if(this.stop=='1'){
+					Indicator.close()
+
+					return
+				}
+				this.current++;
+				axios.get(`/api/getGoods?page=${this.current}&zy_ids=p8_c4_l4_1456_1186_1220_1406_1184_1217_1371_5_128_106_51_18_1391&app_name=zhe&catname=newest_zhe`).then(res=>{
+					console.log(res.data.data)
+					 this.stop = res.data.data.has_more_page
+					 this.datalist=[...this.datalist,...res.data.data.goods]
+
+				})
+				
+			}
+		}
+	}
+
+</script>
+
+
+<style type="text/css" lang="scss">
+	.homenav_list{
+		display:flex;
+		flex-flow:wrap;
+		justify-content: space-between;
+		.homenav_li{
+			margin-top:5px;
+			width:49.5%;
+			
+			.homenav_img{
+				width:100%;
+			}
+			.homenav_price{
+				display:inline-block;
+				margin-top:10px;
+				text-indent:5px;
+				font-size:14px;
+				color:#ff464e;
+			}
+			.homenav_allprice{
+				color:#999;
+				text-decoration:line-through;
+			}
+			.homenav_sport{
+				display:block;
+				color:#999;
+				line-height:30px;
+				text-indent:10px;
+			}
+			.homenav_vip{
+				font-size:13px;
+				text-indent:5px;
+				margin-bottom:10px;
+				color:#3b3b3b;
+			}
+		}
+	} 
+
+
+</style>
